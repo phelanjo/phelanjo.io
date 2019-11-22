10109166b23c421e822b5173f9921681953c8b7e
Johnny Phelan <phelanjo@oregonstate.edu>
2019-11-22 15:02:37 -0800

    Create ImageCard component

---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Photo from '../assets/photo.jpg'
+import '../stylesheets/ImageCard.css'
+
+export class ImageCard extends Component {
+    render() {
+        return (
+            <div class="ic">
+                <img class="ic" src={Photo} />
+                <text class="name">Johnny Phelan</text>
+                <text class="info">Software Developer</text>
+                <text class="info">Student, OSU-Cascades</text>
+            </div>
+        )
+    }
+}
