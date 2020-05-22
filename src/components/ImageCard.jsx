052f78e988389f91ef85b7278a2b10a7dc1a737b
Johnny Phelan <phelanjo@oregonstate.edu>
2020-05-22 09:50:21 -0700

    Add alt text to image tag

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -6,7 +6,7 @@ export class ImageCard extends Component {
     render() {
         return (
             <div class="ic">
-                <img class="ic" src={Photo} />
+                <img class="ic" src={Photo} alt="johnny.png" />
                 <text class="name">Johnny Phelan</text>
                 <text class="info">Software Developer</text>
                 <text class="info">Student, OSU-Cascades</text>
