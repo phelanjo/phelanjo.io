41b1f83cee41c2fc31dac1746260ac90f82a31c2
Johnny Phelan <phelanjo@oregonstate.edu>
2020-05-23 10:48:22 -0700

    Display nav bar correctly on smaller screens.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,37 @@
-import React, { Component } from 'react'
-import '../stylesheets/NavBar.css'
+import React, { Component } from "react";
+import "../stylesheets/NavBar.css";
 
 export class NavBar extends Component {
   render() {
-    return(
+    return (
       <div class="nav">
         <ul class="nav">
           <li class="nav">
-            <a class="nav" href="https://phelan-resume.s3.amazonaws.com/phelan_res.pdf">
-              <i class="far fa-file"></i> Resume
+            <a
+              class="nav"
+              href="https://phelan-resume.s3.amazonaws.com/phelan_res.pdf"
+            >
+              <i class="far fa-file"></i>
+              <text class="nav">Resume</text>
             </a>
           </li>
           <li class="nav">
             <a class="nav" href="https://www.github.com/phelanjo">
-              <i class="fab fa-github"></i> GitHub
+              <i class="fab fa-github"></i>
+              <text class="nav">GitHub</text>
             </a>
           </li>
           <li class="nav">
-            <a class="nav" href="https://www.linkedin.com/in/johnny-phelan-2981491aa/">
-              <i class="fab fa-linkedin"></i> LinkedIn
+            <a
+              class="nav"
+              href="https://www.linkedin.com/in/johnny-phelan-2981491aa/"
+            >
+              <i class="fab fa-linkedin"></i>
+              <text class="nav">LinkedIn</text>
             </a>
           </li>
         </ul>
       </div>
-    )
+    );
   }
 }
