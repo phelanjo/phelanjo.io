27d4cab5f80cfd39016351d9c1c7f99f1c6e4f95
Johnny Phelan <phelanjo@oregonstate.edu>
2020-05-22 17:46:38 -0700

    Fix indentation and add logo icons to buttons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,21 +2,27 @@ import React, { Component } from 'react'
 import '../stylesheets/NavBar.css'
 
 export class NavBar extends Component {
-    render() {
-        return(
-            <div class="nav">
-                <ul class="nav">
-                    <li class="nav">
-                        <a class="nav" href="https://phelan-resume.s3.amazonaws.com/phelan_res.pdf">Resume</a>
-                    </li>
-                    <li class="nav">
-                        <a class="nav" href="https://www.github.com/phelanjo">GitHub</a>
-                    </li>
-                    <li class="nav">
-                        <a class="nav" href="http://spootify.s3-website-us-east-1.amazonaws.com">Spootify</a>
-                    </li>
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return(
+      <div class="nav">
+        <ul class="nav">
+          <li class="nav">
+            <a class="nav" href="https://phelan-resume.s3.amazonaws.com/phelan_res.pdf">
+              <i class="far fa-file"></i> Resume
+            </a>
+          </li>
+          <li class="nav">
+            <a class="nav" href="https://www.github.com/phelanjo">
+              <i class="fab fa-github"></i> GitHub
+            </a>
+          </li>
+          <li class="nav">
+            <a class="nav" href="https://www.linkedin.com/in/johnny-phelan-2981491aa/">
+              <i class="fab fa-linkedin"></i> LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
 }
