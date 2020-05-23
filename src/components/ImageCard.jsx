6b32498726b96257265723e3e50b57ba473595f2
Johnny Phelan <phelanjo@oregonstate.edu>
2020-05-22 17:46:49 -0700

    Fix indentation

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import Photo from '../assets/photo.jpg'
-import '../stylesheets/ImageCard.css'
+import React, { Component } from "react";
+import Photo from "../assets/photo.jpg";
+import "../stylesheets/ImageCard.css";
 
 export class ImageCard extends Component {
-    render() {
-        return (
-            <div class="ic">
-                <img class="ic" src={Photo} alt="johnny.png" />
-                <text class="name">Johnny Phelan</text>
-                <text class="info">Software Developer</text>
-                <text class="info">Student, OSU-Cascades</text>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div class="ic">
+        <img class="ic" src={Photo} alt="johnny.png" />
+        <text class="name">Johnny Phelan</text>
+        <text class="info">Software Developer</text>
+        <text class="info">Student, OSU-Cascades</text>
+      </div>
+    );
+  }
 }
